4b07a06aab36f434c8aad7a29aa0f04091e73940
ClementBu <clement.burgaud@gmail.com>
2014-10-16 14:12:27 +0200

    test commit

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
         int A[][] = new int [100][50];
         int B[][] = new int [100][50];
-
+        System.out.println("Kuga?");
         Tableau a = new Tableau(A);
         Tableau b = new Tableau(B);
 
