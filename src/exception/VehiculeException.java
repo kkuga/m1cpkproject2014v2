9cced0ac737c517611647c243598a73e0eecb573
kuga <k.kugasanth@gmail.com>
2014-11-12 18:48:39 +0100

    ajout Exception et package test

---
+++ src/exception/VehiculeException.java
@@ -0,0 +1,10 @@
+package exception;
+
+/**
+ * Created by admin on 11/11/2014.
+ */
+public class VehiculeException extends Exception {
+        public VehiculeException(String msg){
+            super(msg);
+        }
+}
---
+++ tests/environnement/MaillonTest.java
@@ -0,0 +1,53 @@
+package environnement;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MaillonTest {
+
+    Maillon m1;
+    private Route[] routes = new Route[3];
+    private Carrefour[] carrefours = new Carrefour[4];
+
+    @BeforeTest
+    public void create(){
+        routes[0] = new Route(1, Direction.DROITE, "Route1");
+        m1=new Maillon(routes[0]);
+        routes[1] = new Route(1, Direction.DROITE, "Route2");
+        routes[2] = new Route(1, Direction.BAS, "Route3");
+
+        carrefours[0] = new Carrefour(routes[0]);
+        carrefours[1] = new Carrefour(routes[1]);
+        carrefours[2] = new Carrefour(routes[2]);
+        carrefours[4] = new Carrefour(routes[3]);
+
+        routes[0].setCarrefour(carrefours[1]);
+        routes[1].setCarrefour(carrefours[2]);
+        routes[2].setCarrefour(carrefours[3]);
+
+        carrefours[4].addRoute(routes[5]);
+        System.out.println(routes[1].getLongueur());
+
+    }
+
+    //Test si on arrive a la fin de la route
+    @Test
+    public void testRouteFin(){
+      // System.out.println(routes[0].estDernierMaillon(m1));
+       Assert.assertEquals(true,routes[0].estDernierMaillon(m1));
+    }
+
+    //Tester si la route suivante a une direction Bas
+    //à completer
+    @Test
+    public void testDirection(){
+        System.out.println(routes[0].estDernierMaillon(m1));
+        Assert.assertEquals(Direction.BAS,routes[2]);
+    }
+
+
+
+}
